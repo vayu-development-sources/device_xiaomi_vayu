c3a4d7cb4ad810c25cee6e789e768c8ad37a046b
xyzuan <xyzuannihboss@gmail.com>
2022-08-17 10:15:38 +0000

    vayu: [TEMP-REVERT] power: Update vayu power hal from AOSP android-13.0.0_r3
    This reverts commit ff4172fe9e51c3833d076527c0627aeee8a6d175.
    
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- power-libperfmgr/aidl/PowerExt.h
+++ power-libperfmgr/aidl/PowerExt.h
@@ -16,14 +16,13 @@
 
 #pragma once
 
-#include <aidl/google/hardware/power/extension/pixel/BnPowerExt.h>
-#include <perfmgr/HintManager.h>
-
 #include <atomic>
 #include <memory>
 #include <thread>
 
-#include "adaptivecpu/AdaptiveCpu.h"
+#include <aidl/google/hardware/power/extension/pixel/BnPowerExt.h>
+#include <perfmgr/HintManager.h>
+
 #include "disp-power/DisplayLowPower.h"
 
 namespace aidl {
@@ -33,18 +32,20 @@ namespace power {
 namespace impl {
 namespace pixel {
 
+using ::android::perfmgr::HintManager;
+
 class PowerExt : public ::aidl::google::hardware::power::extension::pixel::BnPowerExt {
   public:
-    PowerExt(std::shared_ptr<DisplayLowPower> dlpw, std::shared_ptr<AdaptiveCpu> acpu)
-        : mDisplayLowPower(dlpw), mAdaptiveCpu(acpu) {}
+    PowerExt(std::shared_ptr<HintManager> hm, std::shared_ptr<DisplayLowPower> dlpw)
+        : mHintManager(hm), mDisplayLowPower(dlpw) {}
     ndk::ScopedAStatus setMode(const std::string &mode, bool enabled) override;
     ndk::ScopedAStatus isModeSupported(const std::string &mode, bool *_aidl_return) override;
     ndk::ScopedAStatus setBoost(const std::string &boost, int32_t durationMs) override;
     ndk::ScopedAStatus isBoostSupported(const std::string &boost, bool *_aidl_return) override;
 
   private:
+    std::shared_ptr<HintManager> mHintManager;
     std::shared_ptr<DisplayLowPower> mDisplayLowPower;
-    std::shared_ptr<AdaptiveCpu> mAdaptiveCpu;
 };
 
 }  // namespace pixel
