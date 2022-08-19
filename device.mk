5db62e3249f131d24f83c2acf26104818ffc6403
tarsin <yuanqingxiang233@163.com>
2022-08-19 11:03:33 +0000

    vayu: Make shim for com.qti.node.watermark.so
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7
    Signed-off-by: bagaskara815 <bagaskara815@gmail.com>

--- device.mk
+++ device.mk
@@ -442,6 +442,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Shim
+PRODUCT_PACKAGES += \
+    libwatermark_shim
+
 # Remove unwanted packages
 PRODUCT_PACKAGES += \
     RemovePackagesVayu
