a8e74c485a85b9e650d0f7341ce2c70450da4c92
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-08-26 06:55:32 +0300

    AlphaDroid Build Time !

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_vayu.mk
+    $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_vayu-eng \
-    derp_vayu-userdebug \
-    derp_vayu-user
+    lineage_vayu-eng \
+    lineage_vayu-userdebug \
+    lineage_vayu-user
