1176aebefb6088ac7b0eee1ed85c3b4bcbdb633c
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-09-15 08:42:23 +0000

    Add BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,6 +171,7 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
 # Vendor Security patch level
 VENDOR_SECURITY_PATCH := 2023-01-01
