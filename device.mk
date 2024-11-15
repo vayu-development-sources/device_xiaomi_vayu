0a4bf82b51e767c3e2eb2f7a8ef559b40cc08570
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-11-15 18:42:49 +0000

    Add Missing LineageOS Overlay

--- device.mk
+++ device.mk
@@ -450,18 +450,13 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3-service-qti-vayu.rc
 
 # Overlays
-PRODUCT_PACKAGES += \
-   CarrierConfigOverlayVayu \
-   DialerOverlayVayu \
-   FlashOverlay \
-   FrameworksResOverlayVayu \
-   NotchBarKillerVayu \
-   SettingsOverlayVayu \
-   SettingsProviderOverlayVayu \
-   SystemUIOverlayVayu \
-   TelephonyOverlayVayu \
-   TetheringConfigOverlayVayu \
-   WifiOverlayVayu
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
+
 
 # Power
 PRODUCT_PACKAGES += \
