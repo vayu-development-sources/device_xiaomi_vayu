480beb77b3c61a5cbe275d585ff1b4686be00655
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-11-27 11:25:09 +0000

    Adapt for Rising OS

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -1,41 +1,45 @@
 #
-# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 The ArrowOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Inherit common products
+
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Lineage configurations
+# Inherit some common RisingOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Sakura flags
-SAKURA_OFFICIAL := true
-SAKURA_MAINTAINER := ArmSM
-TARGET_BOOT_ANIMATION_RES := 1080
-SAKURA_BUILD_TYPE := gapps
-
-# Device identifier
-PRODUCT_BRAND := POCO
-PRODUCT_MODEL := POCO X3 Pro
+PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_vayu
-
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_SYSTEM_NAME := vayu_global
 PRODUCT_SYSTEM_DEVICE := vayu
 
-# GMS
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+# Ship GApps
+
 WITH_GMS := true
 
+# RisingOS flags
+PRODUCT_NO_CAMERA := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+TARGET_ENABLE_PIXEL_FEATURES := true
+RISING_MAINTAINER := momenabdulrazekali
+RISING_BUILDTYPE := UNOFFICIAL
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="momenabdulrazekali"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 13 TKQ1.221013.002 V14.0.3.0.TJUMIXM release-keys" \
-    PRODUCT_MODEL=M2102J20SG
+    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
+    BuildDesc="vayu_global-user 13 RKQ1.200826.002 V14.0.3.0.TJUMIXM release-keys" \
+    BuildFingerprint=Xiaomi/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
 
-BUILD_FINGERPRINT := POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
