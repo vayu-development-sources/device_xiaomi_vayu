c732d84e1ab9e58f185f3d6e7936e1a49fcd0ed0
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-11-30 13:09:03 +0000

    vayu: remove dolby inheritance

--- device.mk
+++ device.mk
@@ -36,9 +36,6 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Dolby
-$(call inherit-product-if-exists, vendor/dolby/dolby.mk)
-
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
