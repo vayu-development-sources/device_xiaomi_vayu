d0c14d1a1eb82ee5b6e75b5df98b910a91248b58
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-12-01 12:41:44 +0000

    Adapt to Project Infinity X

--- infinity_vayu.mk
+++ infinity_vayu.mk
@@ -8,32 +8,39 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common Project infinity stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-EVO_BUILD_TYPE := Unofficial
-TARGET_DISABLE_EPPE := true
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_APERTURE_CAMERA := false
-
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2340
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-# Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_vayu
+# Device identifier
+PRODUCT_NAME := infinity_vayu
 PRODUCT_DEVICE := vayu
-PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_SYSTEM_NAME := vayu_global
+PRODUCT_SYSTEM_DEVICE := vayu
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceName=$(PRODUCT_SYSTEM_DEVICE) \
-    DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
     BuildDesc="vayu_global-user 13 RKQ1.200826.002 V14.0.3.0.TJUMIXM release-keys" \
     BuildFingerprint=Xiaomi/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
 
+# INFINITY-X FLAGS
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := momenabdulrazekali
+WITH_GAPPS := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_BLUR := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_SUPPORTS_QUICK_TAP := true
+USE_MOTO_CALCULATOR := false
+TARGET_BUILD_VIMUSIC := false
