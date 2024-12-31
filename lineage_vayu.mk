8eb8ec59a45e7047dfdd785366962b4e064f528c
momenabdulrazek <momen.abdelrazek33@gmail.com>
2024-12-31 23:23:27 +0000

    hello mistOS

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -11,9 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit some common RisingOS stuff.
+# Inherit some common MistOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+
+# Build stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_EXCLUDES_AUDIOFX := true
+WITH_GMS := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := false
+TARGET_DEFAULT_PIXEL_LAUNCHER := false
+MISTOS_MAINTAINER=momenabdulrazekali
+MIST_BUILD_TYPE := UNOFFICIAL
+TARGET_ENABLE_BLUR := true
+
+# Device identifiers
 PRODUCT_NAME := lineage_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
@@ -22,18 +35,6 @@ PRODUCT_MODEL := POCO X3 Pro
 PRODUCT_SYSTEM_NAME := vayu_global
 PRODUCT_SYSTEM_DEVICE := vayu
 
-# Ship GApps
-
-WITH_GMS := true
-
-# RisingOS flags
-PRODUCT_NO_CAMERA := true
-TARGET_DEFAULT_PIXEL_LAUNCHER := false
-TARGET_ENABLE_PIXEL_FEATURES := true
-RISING_MAINTAINER := momenabdulrazekali
-RISING_BUILDTYPE := UNOFFICIAL
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingMaintainer="momenabdulrazekali"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
@@ -42,4 +43,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME) \
     BuildDesc="vayu_global-user 13 RKQ1.200826.002 V14.0.3.0.TJUMIXM release-keys" \
     BuildFingerprint=Xiaomi/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys
-
