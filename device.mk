f3ebd1955ec039cf3158afd1fda7b4a5ca74fc50
momenabdulrazek <momenabdulrazek@users.noreply.github.com>
2025-01-05 19:06:24 +0100

    Readded Non-Offending DRM packages

--- device.mk
+++ device.mk
@@ -220,7 +220,8 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4.vendor \
+    libcrypto-v33
 
 # Fastbootd
 PRODUCT_PACKAGES += \
