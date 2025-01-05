52fa777f2132ed8b213dcac77a52f00ab26ea019
Pranav Vashi <neobuddy89@gmail.com>
2025-01-05 19:01:37 +0100

    vayu: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false

--- device.mk
+++ device.mk
@@ -36,6 +36,9 @@ PRODUCT_TARGET_VNDK_VERSION := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Update
+AB_OTA_UPDATER := false
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
