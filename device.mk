b5f8c9906f6c1c058c872df0a08d8b104fe1dc3b
Michael Bestas <mkbestas@lineageos.org>
2025-01-09 15:14:16 +0000

    vayu: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- device.mk
+++ device.mk
@@ -183,8 +183,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    gralloc.msmnile \
-    hwcomposer.msmnile \
+    gralloc.qcom \
+    hwcomposer.qcom \
     libdisplayconfig.qti \
     libsdmcore \
     libsdmutils \
