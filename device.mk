69840b11dc5a56c8cc380b5e36d3206d5d07ff89
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2025-03-28 20:05:17 +0200

    vayu: product packages: remove offending NFC product packages
    Since they now defined in lineage source so no need to declare them

--- device.mk
+++ device.mk
@@ -556,8 +556,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2.vendor \
     com.android.nfc_extras \
     libchrome.vendor \
-    NfcNci \
-    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
